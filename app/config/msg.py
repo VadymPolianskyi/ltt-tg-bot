afcf3a5253a9ea2ba72b5df7b94b3413a6420c38
Vadym Polianskyi <icvadym.polianskyi@americanwell.com>
2022-01-08 11:34:25 -0600

    LF-18: Correct all messages

--- app/config/msg.py
+++ app/config/msg.py
@@ -11,10 +11,12 @@ LAST_EVENTS_COMMAND = "last\_events"
 STATISTICS_COMMAND = "statistics"
 
 START = f"""
-Hi! I'm the @LifeTimeTrackerBot
+Hi! I'm @LifeTimeTrackerBot
 
-With me you will know how really you spend your life.
-I work with the next commands:
+With me, you will know how really you spend your life.
+Let's analyze your time!
+
+Commands:
 
 /{ACTIVITIES_COMMAND} - show all activities
 /{ADD_ACTIVITY_COMMAND} - create a new activity
@@ -32,7 +34,7 @@ I work with the next commands:
 
 # universal
 
-CHOOSE_ACTIVITY = """Please choose activity:"""
+CHOOSE_ACTIVITY = """Please choose an activity"""
 
 # /activities
 
@@ -41,25 +43,25 @@ ALL_ACTIVITIES = "All your activities: {} \n\n/" + ADD_ACTIVITY_COMMAND + " - ad
 # /add_activity
 
 ADD_ACTIVITY_1 = """Please write the name for a new activity"""
-ADD_ACTIVITY_2 = "Created new activity with name `{}`. Write /" + ACTIVITIES_COMMAND + " to see all your activities."
+ADD_ACTIVITY_2 = "Created new activity with the name `{}`. Write /" + ACTIVITIES_COMMAND + " to see all your activities."
 
 # /delete_activity
 
 DELETE_ACTIVITY_1 = CHOOSE_ACTIVITY
 DELETE_ACTIVITY_2 = """Are you sure that you want to delete the activity `{}`?"""
-DELETE_ACTIVITY_3_1 = "Deleted activity with name `{}`. Write /" + ACTIVITIES_COMMAND + " to see all your activities."
+DELETE_ACTIVITY_3_1 = "Deleted activity with the name `{}`. Write /" + ACTIVITIES_COMMAND + " to see all your activities."
 DELETE_ACTIVITY_4_1 = "Deletion is canceled. Write /" + ACTIVITIES_COMMAND + " to see all your activities."
 
 # /track
 
 TRACK_1 = CHOOSE_ACTIVITY
-TRACK_2 = """Got you. `{}`. How long?"""
+TRACK_2 = """Got you! `{}`. How long? \nExamples: `1h20m`, `10m`, `3h`"""
 FINISHED_TRACKING = """Tracked `{}`. Totally spent {} hours {} minutes."""
 
 # /start_tracking
 
 START_TRACKING_1 = CHOOSE_ACTIVITY
-START_TRACKING_2 = "Started! Please, don't forget to send /" + STOP_COMMAND + " after you finish `{}`. Have fun!"
+START_TRACKING_2 = "Started! Please, don't forget to send /" + STOP_COMMAND + " after you finish the `{}`. Have fun!"
 
 # /stop_tracking
 
@@ -70,7 +72,7 @@ STOP_TRACKING_3_1 = f""" Oh, I can't stop NOTHING. Please start activity by /{ST
 
 # /last_events
 
-LAST_EVENTS_1 = "Please, write a number how many last events you want to see. Max number: `20`."
+LAST_EVENTS_1 = "Please, write a number of how many last events you want to see. Max number: `20`."
 LAST_EVENTS_2 = "Your last `{}` tracked events:\n{}"
 
 # /delete_event
@@ -78,13 +80,12 @@ LAST_EVENTS_2 = "Your last `{}` tracked events:\n{}"
 DELETE_EVENT_1 = CHOOSE_ACTIVITY
 DELETE_EVENT_2 = "Please, choose one `{}` event that you want to delete:"
 DELETE_EVENT_3 = "Are you sure that you want to delete this event?"
-DELETE_EVENT_4_1 = "Event deleted"
+DELETE_EVENT_4_1 = "Event deleted."
 DELETE_EVENT_5_1 = "Deletion is canceled."
 
 # /statistics
 
-STATISTIC_1 = "Please write period \n Example: `1D`, `1W`, `1M` for last time range, or `01.01.2021 - 05.01.2021`"
-STATISTIC_2 = "That's how was your {}"
+STATISTIC_1 = "Please write period \nExamples: `1D`, `1W`, `1M` for last time range, or `01.01.2021 - 05.01.2021`"
 
 # ERRORS
 
